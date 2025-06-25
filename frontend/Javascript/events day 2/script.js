568e47517443818137158ee4ba195d11005d5829
prasadkadam1 <136068760+prasadkadam1@users.noreply.github.com>
2025-06-25 08:30:28 +0530

    events day 2

---
+++ frontend/Javascript/events day 2/script.js
@@ -0,0 +1,23 @@
+// let parachute = document.getElementById("parachute");
+// let body = document.body;
+// body.addEventListener("keypress", (eventObj) => {
+//   console.log(eventObj);
+// });
+
+// ===============================
+
+// let btn = document.querySelector('button')
+// btn.addEventListener('click', (eventObj)=>{
+//     eventObj.target.style.backgroundColor = "crimson"
+//     eventObj.target.style.border = "none"
+//     eventObj.target.style.padding = "10px 20px"
+//     eventObj.target.style.color = "white"
+// })
+
+// ===============================
+
+// let input = document.querySelector("input");
+// let heading = document.querySelector("h1");
+// input.addEventListener("input", (e) => {
+//   heading.innerText = e.target.value;
+// });
