007ac0d98e9162643802a2d2226f33ac95d17acd
prasadkadam1 <136068760+prasadkadam1@users.noreply.github.com>
2025-09-09 08:16:15 +0530

    Update Crud3.jsx
    create and read operation

--- frontend/react/react_project/src/components/crud/tasks/Crud3.jsx
+++ frontend/react/react_project/src/components/crud/tasks/Crud3.jsx
@@ -1,37 +1,71 @@
 import { useState } from "react";
 
-const Crud3 = () => {
-  let [uName, setUName] = useState("");
-  let [pass, setPass] = useState("");
-  let [list, setList] = useState([{}, {}, {}]);
-  const handleClick = (e) => {
-    e.preventDefault();
-    let temp = {
-      username: uName,
-      password: pass,
-    };
-    setList([...list, temp]);
-    console.log(list);
+let Crud3 = () => {
+  let [state, setState] = useState({
+    username: "",
+    password: "",
+    id: Date.now(),
+    list: [
+      {
+        username: "abc",
+        password: 1234,
+        id: 394863248762387,
+      },
+    ],
+  });
+  let handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
   };
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    let tempObj = {
+      username: state.username,
+      password: state.password,
+      id: state.id,
+      // list : state.list //! it will create a nested unnecessary arrays.
+    };
+    setState({
+      list: [...state.list, tempObj],
+      username: "",
+      password: "",
+      id: Date.now(),
+    });
+  };
+  console.log(state);
   return (
     <div>
+      //! create operation
       <form action="">
         <input
           type="text"
-          placeholder="Enter Username"
-          onChange={(e) => {
-            setUName(e.target.value);
-          }}
+          placeholder=""
+          name="username"
+          value={state.username}
+          onChange={handleChange}
         />
         <input
           type="text"
-          placeholder="Enter Password"
-          onChange={(e) => {
-            setPass(e.target.value);
-          }}
+          placeholder=""
+          name="password"
+          value={state.password}
+          onChange={handleChange}
         />
-        <button onClick={handleClick}>Click</button>
+        <button onClick={handleSubmit}>Create new user</button>
       </form>
+      //! read operation
+      <div>
+        {state.list.map((obj) => {
+          return (
+            <div key={obj.id}>
+              <p>Username : {obj.username}</p>
+              <p>Password : {obj.password}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
